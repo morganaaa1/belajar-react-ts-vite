d716fcd09fa0ba5aa08a452c03711479d7014b30
ali <alinabilahramadhan@gmail.com>
2023-07-31 20:04:43 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const App: React.FC = () => {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-200 to-pink-300">
+      Hello World!
+    </div>
+  )
+
+}
+
+export default App
